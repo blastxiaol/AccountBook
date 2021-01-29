b3494ab224a8365a6d52bd6b30fb4c7ad466f301
Administrator <506100377@qq.com>
2021-01-29 22:41:36 +0800

    new change

--- src/AccountBookMainFunction.java
+++ src/AccountBookMainFunction.java
@@ -1,5 +1,5 @@
 public class AccountBookMainFunction {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello world123");
     }
 }
